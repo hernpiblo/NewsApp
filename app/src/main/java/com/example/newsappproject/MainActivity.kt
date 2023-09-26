d1d30aff352dba71d344f4e43baa217e5673b6ef
hernping.lee <hernping.lee@gwmail.gwu.edu>
2023-09-26 16:06:34 -0400

    Add UI for home screen activity

--- app/src/main/java/com/example/newsappproject/MainActivity.kt
+++ app/src/main/java/com/example/newsappproject/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.newsappproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var searchEditText: EditText
+    private lateinit var searchBtn: Button
+    private lateinit var topHeadlinesBtn: Button
+    private lateinit var newsByLocationBtn: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        searchEditText = findViewById(R.id.SearchEditText)
+        searchBtn = findViewById(R.id.SearchButton)
+        topHeadlinesBtn = findViewById(R.id.TopHeadlinesButton)
+        newsByLocationBtn = findViewById(R.id.NewsByLocationButton)
+
+
+
     }
 }
